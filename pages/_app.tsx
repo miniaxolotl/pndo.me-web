63ad62e4542f7ae55f2c3a4996564e5f2e46f915
Elias Mawa <elias@emawa.io>
2020-04-10 08:45:14 -0600

    add root stylesheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,28 +5,30 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { rootState, reducer } from '../store/RootStore';
 
+import '../styles/_style.css'
+
 /**
  * @param initialState The store's initial state (on the client side, the state of the server-side store is passed here)
  */
 const makeStore: MakeStore = (initialState: rootState) => {
-  return createStore(reducer, initialState);
+	return createStore(reducer, initialState);
 };
 
 class MyApp extends App<ReduxWrapperAppProps<RootState>> {
-  static async getInitialProps({ Component, ctx }: AppContext) {
-    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+	static async getInitialProps({ Component, ctx }: AppContext) {
+		const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
 
-    return { pageProps };
-  }
+		return { pageProps };
+	}
 
-  render() {
-    const { Component, pageProps, store } = this.props;
-    return (
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    );
-  }
+	render() {
+		const { Component, pageProps, store } = this.props;
+		return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+		);
+	}
 }
 
 export default withRedux(makeStore)(MyApp);
