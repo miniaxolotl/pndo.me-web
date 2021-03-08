4e9d716ac784b6a0761a86ebe76413d76532e298
Elias Mawa <elias@emawa.io>
2021-03-07 20:40:31 -0700

    clean for write

--- components/Main.tsx
+++
@@ -1,8 +0,0 @@
-import { Flex, StackProps } from '@chakra-ui/react'
-
-export const Main = (props: StackProps) =>{
-	return (
-		<Flex direction="column" width="90vw"
-			justifyContent="center" alignItems="center" {...props} />
-	);
-};
