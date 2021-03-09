f8639fd6c08849862f96e485385e38956d7534c7
Elias Mawa <elias@emawa.io>
2021-03-09 02:07:52 -0700

    use router for masthead link

--- src/components/Masthead.tsx
+++ src/components/Masthead.tsx
@@ -1,6 +1,7 @@
+import Link from 'next/link';
 import { NextPage } from 'next';
 
-import { Box, Heading, Img } from '@chakra-ui/react';
+import { Box, Link as CLink, Heading, Img } from '@chakra-ui/react';
 
 import style from './Masthead.module.css';
 
@@ -9,10 +10,14 @@ interface Props {}
 export const Masthead: NextPage<Props> = (_props: Props) => {
 	return(
 		<Box className={style.masthead}>
-			<Heading as="h1" size='4xl' alignContent='center' justifyContent='center' className={style.heading}>
-				pndo.me
-				<Img src="/logo.svg" height='6rem' className={style.image} alt="pndo.me logo" />
-			</Heading>
+			<CLink as={Link} href='/' _hover={{ textDecoration: 'none', color: 'rgba(0, 0, 0, 0.25)' }}>
+				<a>
+					<Heading as="h1" size='4xl' alignContent='center' justifyContent='center' className={style.heading}>
+						pndo.me
+						<Img src="/logo.svg" height='6rem' className={style.image} alt="pndo.me logo" />
+					</Heading>
+				</a>
+			</CLink>
 		</Box>
 	);
 };
