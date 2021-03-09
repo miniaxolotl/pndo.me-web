00b2b1577e11331431e8b2b0386d3c4ed687f113
Elias Mawa <elias@emawa.io>
2021-03-09 05:17:26 -0700

    fix error on initial run

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 
 import { StoreProvider } from '../lib/store/StoreProvider';
-import { hydrateAllStore } from '../lib/store/store';
+import { createAllStore, hydrateAllStore } from '../lib/store/store';
 
 import fonts from '../styles/font-face';
 import theme from '../styles/default';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	const store = hydrateAllStore(pageProps.state);
+	const state = pageProps.state ? pageProps.state : createAllStore();
+	const store = hydrateAllStore(state);
+	
 	return (
 		<ChakraProvider resetCSS theme={theme}>
 			<StoreProvider store={store}>
