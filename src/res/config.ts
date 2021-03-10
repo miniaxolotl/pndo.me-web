da11db6f823c747472f30f55e999d4b50b5ba045
Elias Mawa <elias@emawa.io>
2021-03-10 01:36:34 -0700

    maxfile

--- src/res/config.ts
+++ src/res/config.ts
@@ -1,7 +1,6 @@
-import { homedir } from "os";
-
 export const config = {
 	site_name: 'pndo.me',
-	server: 'https://home.emawa.io:3032'
+	server: 'https://home.emawa.io:3032',
+	MAX_FILE: (2**20)*500
 };
 
