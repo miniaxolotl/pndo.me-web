2f24e23dc786c8b07026dfb7593918ffee0ee6ad
Elias Mawa <elias@emawa.io>
2021-03-14 17:58:03 -0600

    tweak

--- src/res/config-example.ts
+++ src/res/config-example.ts
@@ -1,11 +1,29 @@
 import { NextSeoProps } from 'next-seo';
 
+interface SiteTheme {
+	/** location of background image */
+	url: string;
+	/** author of background image */
+	source: string;
+}
+
+/** defines sitewide configurations */
 interface ConfigType {
-	canonical: string;
+	/** name of the ite */
 	site_name: string;
+	/** canonical url */
+	canonical: string;
+	/** server url */
 	server: string;
-	MAX_FILE: number;
+	/** max bytes for individual files */
+	MAX_BYTES: number;
+	/** default site wide soe tags */
 	seo: NextSeoProps;
+	/** theme data for website */
+	theme: {
+		dark: SiteTheme;
+		light: SiteTheme;
+	};
 }
 
 export default ConfigType;
